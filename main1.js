51cea2d7a72c87ba933c8727829dc166ebf50008
Christo Kumar <christoslimit@gmail.com>
2021-12-24 14:10:35 +0530

    Added Data types in JS

--- main1.js
+++ main1.js
@@ -65,3 +65,41 @@ function h1(strings, ...values) {
     }
     return "<h1>" + body + "<h1>"
 }
+
+//JavaScript types
+//Dynamically Typed language - Type is determined at the runtime
+//Dynamic vs Reliablity
+function jsTypesDemo(){
+    var a = 'christo';
+    console.log(typeof(a));
+    a = 1;
+    console.log(typeof(a));
+
+    var b;
+    console.log(typeof(b)); //type undefined as its a dynamic type language
+    var c = null; //type is null, which is assigned by developer.
+    //so null is a consciously done by developers to define an empty variable.
+    //however 
+    console.log(typeof(c));
+    console.log(null==undefined); //true
+    console.log(null=== undefined); //false types are different
+    console.log(0=='');//true
+    console.log(0=='0');
+    console.log(0==='0');
+
+    console.log("abc"/4); //NaN
+    console.log(typeof("abc"/4)); //number
+
+    //NaN compared to anything is false
+    //Nan cannot be compared
+    console.log(NaN==1);//false
+    console.log(NaN==false);//false
+    console.log(NaN==NaN);//false
+    console.log(isNaN(Number("A"))); //true
+    console.log(isNaN(NaN));
+    //NaN can be checked correctly by comparing to itself, as its the only type which returns false when compare to iself.
+    var somNum = NaN;
+    console.log(somNum!== somNum);
+}
+jsTypesDemo();
+
